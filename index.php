f6b6f721d69e4364993b9cc2ff96433e681443a3
Nzingani <isabelnzingani@outlook.com>
2018-07-27 10:01:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>P3ex3</title>
+  </head>
+  <body>
+    <?php
+    $variable = 100;
+   $variable1 = 10;
+while($variable > 10)
+{
+echo $variable * $variable1 .' ';
+$variable --;// incrémenter
+}
+  ?>
+</body>
+</html>
